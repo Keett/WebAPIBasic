29760a4fff230d34db7e27682a18ab4c03dccc30
Keett <kubraeliftozkoparan@gmail.com>
2021-08-12 16:37:41 +0300

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private DataContext _context;
+
+        public WeatherForecastController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult> GetSehirler()
+        {
+            var sehirler = _context.Sehirler.ToList();
+            return Ok(sehirler);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetSehir(int id)
+        {
+            var sehirler = _context.Sehirler.FirstOrDefault(x => x.Id == id);
+            return Ok(sehirler);
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication1
+{
+    public class WeatherForecast
+    {
+        //model
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
